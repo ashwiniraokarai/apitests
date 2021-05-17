77d6ee576dea6b2fd1019b9ba1885ce7f8e73270
ashwiniraokarai <ashwini@biggerpockets.com>
2021-05-16 18:06:17 -0600

    expand tests for get requests

--- app/controllers/game_consoles_controller.rb
+++ app/controllers/game_consoles_controller.rb
@@ -1,8 +1,22 @@
 class GameConsolesController < ApplicationController
-  def index
-    render json: { 'status' => 'ok', 'consoles' => [  { name: "NES", manufacturer: "Nintendo" },
-                                                     { name: "XBOX", manufacturer: "Microsoft" },
-                                                     { name: "PS2", manufacturer: "Sony"}]}
 
+  def index
+    consoles = [ { name: "NES", manufacturer: "Nintendo" },
+                 { name: "XBOX", manufacturer: "Microsoft" },
+                 { name: "PS2", manufacturer: "Sony"}]
+
+    if params[:manufacturer]
+      consoles = consoles.select do |console|
+        console[:manufacturer] == params[:manufacturer]
+      end
+    end
+
+    render json: { 'status' => 'ok', 'consoles' => consoles }
   end
+
+  # def create
+  #   consoles.name =
+  #   consoles.manufacturer
+  #   render json: { }
+  # end
 end
--- spec/requests/game_consoles_spec.rb
+++ spec/requests/game_consoles_spec.rb
@@ -1,21 +1,37 @@
 require "rails_helper"
 
 RSpec.describe "game consoles" do
+  let (:json) {JSON.parse(response.body)}
+
   describe "GET game consoles" do
     it "returns a status message" do
       get '/game_consoles'
       expect(response.status).to eq(200)
-
-      json = JSON.parse(response.body)
       expect(json['status']).to eq('ok')
     end
 
     it "returns an array of game consoles" do
       get '/game_consoles'
       expect(response.status).to eq(200)
-
-      json = JSON.parse(response.body)
       expect(json.keys).to match_array(["status", "consoles"])
+
+      json["consoles"].each do |console|
+        expect(console.keys).to match_array(["name", "manufacturer"])
+        expect(console["name"]).not_to be_empty
+        expect(console["manufacturer"]).not_to be_empty
+      end
+    end
+
+    it "returns the consoles for a specified manufacturer" do
+      get '/game_consoles', params: {manufacturer: "Nintendo"}
+      expect(json["consoles"]).to contain_exactly({ "name" => "NES", "manufacturer" => "Nintendo"})
     end
   end
+
+  # describe "POST to game consoles" do
+  #   it "creates and adds a new game console to the list" do
+  #     post "/game_consoles"
+  #     expect(response.status).to eq(201)
+  #   end
+  # end
 end
